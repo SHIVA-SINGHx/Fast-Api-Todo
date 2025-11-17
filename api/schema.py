1a4bf0e569278831f674576f8f857fb93797ebaa
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2025-11-17 09:46:25 -0800

    schema changed

--- api/schema.py
+++ api/schema.py
@@ -2,10 +2,16 @@ from pydantic import BaseModel
 
 class TodoBase(BaseModel):
     task: str
+    description: str | None = None
     completed: bool = False
 
 class TodoCreate(TodoBase):
     pass
+
+class TodoUpdate(BaseModel):
+    task: str | None = None
+    description: str | None = None
+    completed: bool | None = None
 
 class TodoResponse(TodoBase):
     id: int
